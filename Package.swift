c7a5d710fbff1457e01cce0a207f1cb51c718189
Ambrus Tóth <32463042+tothambrus11@users.noreply.github.com>
2026-07-22 22:57:55 +0300

    Update swift-syntax dependency to version 603.0.2

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     .library(name: "Archivist", targets: ["Archivist"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-syntax", from: "602.0.0"),
+    .package(url: "https://github.com/apple/swift-syntax", from: "603.0.2"),
   ],
   targets: [
     .target(
